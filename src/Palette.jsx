64d356493ddbe513768bc430c45158a43f05254c
HADES-01 <adityavats122017@gmail.com>
2021-05-20 01:02:35 +0530

    Created Palette Component

---
+++ src/Palette.jsx
@@ -0,0 +1,14 @@
+import { React, Component } from "react";
+
+class Palette extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Palette</h1>
+        <p>This is a Palette Component</p>
+      </div>
+    );
+  }
+}
+
+export default Palette;
