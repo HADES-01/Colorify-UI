f3f191ea205c5188d3fe2ac81ea81f995653304f
HADES-01 <adityavats122017@gmail.com>
2021-05-20 16:51:31 +0530

    Added Color Shades Helper
    Signed-off-by: HADES-01 <adityavats122017@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import "./App.css";
 import Palette from "./Palette.jsx";
 import seedColors from "./seedColors";
 
+import { generatePalette } from "./Helpers";
+
 function App() {
+  console.log(generatePalette(seedColors[2]));
   return (
     <div className="App">
       <Palette {...seedColors[2]} />
