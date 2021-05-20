f8da8cc94086e575597fd29fa0584ec75943fae4
HADES-01 <adityavats122017@gmail.com>
2021-05-20 18:03:20 +0530

    Added Slider for Color Scale
    Signed-off-by: HADES-01 <adityavats122017@gmail.com>

--- src/Palette.jsx
+++ src/Palette.jsx
@@ -1,13 +1,33 @@
 import { React, Component } from "react";
 import ColorBox from "./ColorBox";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+
 import "./Palette.css";
 class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { level: 500 };
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+  changeLevel(level) {
+    this.setState({ level: level });
+  }
   render() {
-    let colorBoxes = this.props.colors.map((color) => (
-      <ColorBox background={color.color} name={color.name} key={color.name} />
+    let { colors } = this.props.palette;
+    let { level } = this.state;
+    let colorBoxes = colors[this.state.level].map((color) => (
+      <ColorBox background={color.hex} name={color.name} key={color.name} />
     ));
     return (
       <div className="Palette">
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={this.changeLevel}
+        />
         <div className="Palette-colors">{colorBoxes}</div>
       </div>
     );
