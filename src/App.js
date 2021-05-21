56af2f246139502189332bf75aee390e6e45986c
HADES-01 <adityavats122017@gmail.com>
2021-05-21 15:23:36 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
 import Palette from "./Palette.jsx";
 import seedColors from "./seedColors";
+import { Switch, Route } from "react-router-dom";
 
 import { generatePalette } from "./Helpers";
 
 function App() {
   return (
     <div className="App">
-      <Palette palette={generatePalette(seedColors[0])} />
+      <Switch>
+        <Route exact path="/" render={() => <h1>Home Route</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={() => <h1>Individual Component</h1>}
+        />
+      </Switch>
+      {/* <Palette palette={generatePalette(seedColors[0])} /> */}
     </div>
   );
 }
