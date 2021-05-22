2a469f87e51a23d3c96824831ad7b1efc7467734
HADES-01 <adityavats122017@gmail.com>
2021-05-22 17:46:20 +0530

    Added DraggableColorBox Component

--- src/NewPaletteForm.jsx
+++ src/NewPaletteForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import clsx from "clsx";
-import { makeStyles } from "@material-ui/core/styles";
+import useStyles from "./styles/NewPaletteFormStyles";
 import Drawer from "@material-ui/core/Drawer";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
@@ -9,70 +9,17 @@ import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import Button from "@material-ui/core/Button";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-  },
-  appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  hide: {
-    display: "none",
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end",
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: -drawerWidth,
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  },
-}));
+import DrggableColorBox from "./DraggableColorBox";
+import { ChromePicker } from "react-color";
+import DraggableColorBox from "./DraggableColorBox";
 
 export default function NewPaletteForm() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(true);
+  const [color, setColor] = React.useState("red");
+  const [colors, setColors] = React.useState(["red", "#e23456"]);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -80,6 +27,14 @@ export default function NewPaletteForm() {
 
   const handleDrawerClose = () => {
     setOpen(false);
+  };
+
+  const handleColorChange = (color) => {
+    setColor(color.hex);
+  };
+
+  const addColor = (color) => {
+    setColors([...colors, color]);
   };
 
   return (
@@ -121,13 +76,34 @@ export default function NewPaletteForm() {
           </IconButton>
         </div>
         <Divider />
+        <Typography variant="h4">Design Your Palette</Typography>
+        <div>
+          <Button variant="contained" color="secondary">
+            Create
+          </Button>
+          <Button variant="contained" color="secondary">
+            Random
+          </Button>
+        </div>
+        <ChromePicker color={color} onChange={handleColorChange} />
+        <Button
+          variant="contained"
+          style={{ backgroundColor: color }}
+          onClick={() => addColor(color)}
+        >
+          Add Color
+        </Button>
       </Drawer>
       <main
+        style={{ padding: "none", margin: "none" }}
         className={clsx(classes.content, {
           [classes.contentShift]: open,
         })}
       >
         <div className={classes.drawerHeader} />
+        {colors.map((col) => (
+          <DraggableColorBox color={col} />
+        ))}
       </main>
     </div>
   );
